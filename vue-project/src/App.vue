3b8a295de396c019e8bb9b219963b7c55a097643
susheelaR1109 <150230816+susheelaR1109@users.noreply.github.com>
2024-01-29 11:31:07 +0530

    implemented vue components and passing of data

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,49 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
   <div id="app">
     <header>
       <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-      </div>
+      <h1>Todo App</h1>
     </header>
 
     <main>
-      <TheWelcome />
+      <TodoList :list="tasks"/>
     </main>
   </div>
 </template>
 
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+    name: 'App',
+    components: {
+      TodoList,
+    },
+    data () {
+      return {
+        tasks: [
+          {
+            title: 'Task 1',
+            status: 'pending'
+
+          },
+          {
+            title: 'Task 2',
+            status: 'completed'
+
+          },
+          {
+            title: 'Task 3',
+            status: 'pending'
+
+          },
+          {
+            title: 'Task 4',
+            status: 'pending'
+
+          }
+        ]
+      }
+    }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+header{
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 10px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ vue-project/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="list-item">
+            <div class="task-title">{{ title }}</div>
+            <button>Add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        title: String,
+        status: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style >
